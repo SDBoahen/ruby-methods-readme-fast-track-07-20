63d4c714e24717a927af30181818d72516c32f1d
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-07-22 00:59:13 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,35 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+# phrase = "Hello World!"
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+# puts phrase
+
+
+def
+
+  say_hello_ten_times
+  
+  phrase = "Hello World!"
+  
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+
+end
+
+say_hello_ten_times
